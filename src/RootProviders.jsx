7e8ee2044b64e045820befb0d1f5d740b2eb45e6
Tekai_ <vip007_007@mail.ru>
2023-09-17 11:36:50 +0300

    Added redux-persist

--- src/RootProviders.jsx
+++ src/RootProviders.jsx
@@ -2,6 +2,8 @@ import App from "./App.jsx";
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./store/index";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store/index.js";
 // сущности роутинга
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 export default function RootProviders() {
@@ -11,12 +13,14 @@ export default function RootProviders() {
       <Provider store={store}>
         {/* подключение роутинга */}
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />}>
-              {/* задаем параметры роутинга */}
-              <Route path=":filter" element={<App />} />
-            </Route>
-          </Routes>
+          <PersistGate loading={null} persistor={persistor}>
+            <Routes>
+              <Route path="/" element={<App />}>
+                {/* задаем параметры роутинга */}
+                <Route path=":filter" element={<App />} />
+              </Route>
+            </Routes>
+          </PersistGate>
         </BrowserRouter>
       </Provider>
     </React.StrictMode>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,44 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { logger } from "redux-logger";
-import { rootReducer } from "./Root-reducer";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+
 import { todosSlice } from "./todos/todos-slice";
 import { filtersSlice } from "./filters/filters-slice";
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const rootReducer = combineReducers({
+  todo: todosSlice.reducer,
+  filter: filtersSlice.reducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//
 
 export const store = configureStore({
-  reducer: {
-    todo: todosSlice.reducer,
-    filter: filtersSlice.reducer,
-  },
+  reducer: persistedReducer,
   devTools: true,
-  middleware: (getDeafaultMiddleware) => getDeafaultMiddleware().concat(logger),
+  middleware: (getDeafaultMiddleware) => {
+    return getDeafaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger);
+  },
   preloadedState: {
     todo: [
       {
@@ -21,3 +49,5 @@ export const store = configureStore({
     ],
   },
 });
+
+export const persistor = persistStore(store);
